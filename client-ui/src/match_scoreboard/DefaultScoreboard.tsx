a39b318a22b88490f7869e6c1f9f291c798b9a19
John Arthur <jarthur@dispersive.io>
2026-05-25 14:36:58 -0400

    wip: restarting on UI using sveltekit

--- client-ui/src/match_scoreboard/DefaultScoreboard.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import {Scoreboard} from "./Scoreboard";
-
-export function DefaultScoreboard() {
-    return <div style={{background: "#f0f0f0", height: "100vh"}}>
-        <Scoreboard/>
-    </div>
-
-}
--- client-ui/src/model/facility.tsx
+++
@@ -1,8 +0,0 @@
-export type Facility = {
-    id?: string;
-    user_id?: string;
-    name: string;
-    address: string;
-    courts: number;
-    layout_photo: string;
-}
